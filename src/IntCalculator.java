e0a0bc4ee1785947e00a58eeae0812027991a3a3
nicobrezai <nicoletastoica1022@yahoo.com>
2025-04-04 16:24:48 +0300

    al doilea commit

--- src/IntCalculator.java
+++ src/IntCalculator.java
@@ -35,3 +35,78 @@ public class IntCalculator
         return this;
  }
 }
+
+abstract class ACalculator<T extends Number> {
+    protected T state;
+
+    // Constructor
+    public ACalculator(T initialValue) {
+        this.state = initialValue;
+    }
+
+    // Result method
+    public T result() {
+        return this.state;
+    }
+
+    // Clear method
+    public void clear() {
+        this.state = init();
+    }
+
+    // Abstract method for initialization
+    protected abstract T init();
+}
+
+class NewIntCalculator extends ACalculator<Integer> {
+    public NewIntCalculator(Integer initialValue) {
+        super(initialValue);
+    }
+
+    @Override
+    protected Integer init() {
+        return 0;
+    }
+
+    public NewIntCalculator add(int value) {
+        this.state += value;
+        return this;
+    }
+
+    public NewIntCalculator subtract(int value) {
+        this.state -= value;
+        return this;
+    }
+
+    public NewIntCalculator multiply(int value) {
+        this.state *= value;
+        return this;
+    }
+}
+
+class DoubleCalculator extends ACalculator<Double> {
+    public DoubleCalculator(Double initialValue) {
+        super(initialValue);
+    }
+
+    @Override
+    protected Double init() {
+        return 0.0;
+    }
+
+    public DoubleCalculator add(double value) {
+        this.state += value;
+        return this;
+    }
+
+    public DoubleCalculator subtract(double value) {
+        this.state -= value;
+        return this;
+    }
+
+    public DoubleCalculator multiply(double value) {
+        this.state *= value;
+        return this;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         IntCalculator calculator = new IntCalculator(10);
         int result = calculator.add(5).subtract(3).multiply(2).result();
-        System.out.println("a) " + result);
-}
+        System.out.println("Int Result: " + result);
+        DoubleCalculator doubleCalculator = new DoubleCalculator(10.0);
+        double doubleResult = doubleCalculator.add(5).subtract(3.3).multiply(2.2).result();
+        System.out.println("Double Result: " + doubleResult);
+
+    }
 }
